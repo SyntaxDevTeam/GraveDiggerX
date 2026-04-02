65d41375a5b19a82ab4d0c54647bab92f3717f48
SyntaxDevTeam <wieszczy85@gmail.com>
2026-04-02 20:49:32 +0200

    update: all dependencies and libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,18 +31,18 @@ dependencies {
     compileOnly("pl.syntaxdevteam:messageHandler-paper:1.1.2-R0.1-SNAPSHOT")
     compileOnly("com.github.ben-manes.caffeine:caffeine:3.2.3")
     compileOnly("com.zaxxer:HikariCP:7.0.2")
-    compileOnly("org.xerial:sqlite-jdbc:3.51.1.0")
-    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.7")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.16-SNAPSHOT")
+    compileOnly("org.xerial:sqlite-jdbc:3.51.3.0")
+    compileOnly("org.mariadb.jdbc:mariadb-java-client:3.5.8")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.16")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
     testImplementation("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     testImplementation("pl.syntaxdevteam:core:1.3.0-R0.2-SNAPSHOT")
     testImplementation("com.zaxxer:HikariCP:7.0.2")
-    testImplementation("org.xerial:sqlite-jdbc:3.51.1.0")
-    testImplementation("org.mockito:mockito-core:5.20.0")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:6.1.0")
-    mockitoAgent("org.mockito:mockito-core:5.20.0") {
+    testImplementation("org.xerial:sqlite-jdbc:3.51.3.0")
+    testImplementation("org.mockito:mockito-core:5.23.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
+    mockitoAgent("org.mockito:mockito-core:5.23.0") {
         isTransitive = false
     }
     testRuntimeOnly("org.slf4j:slf4j-simple:2.0.17")
